8da44fd38d928e847850faf5b95293045ef88a6f
emil <emilm7843@gmail.com>
2021-09-28 15:35:47 +0200

    initial

---
+++ tagi/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace tagi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var tagsData = new List<TagData>();
+            for (int i = 1; i <= 10; i++) //the API does not allow queries larger than 100 in one batch
+            {
+               tagsData= tagsData.Concat(new TagDataDownloader(page: 1).Download()).ToList();
+            }
+
+            ViewData["tagsArray"] = tagsData;
+            ViewData["allUses"] = tagsData.Sum(data => data.count);
+            return View("Index");
+        }
+
+       
+        
+    }
+}
---
+++ tagi/TagDataDownloader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using RestSharp;
+using System.Text.Json;
+using System.IO;
+namespace tagi
+{
+    public class TagDataDownloader
+    {
+        private RestClient client;
+        private string site;
+        private int page;
+        public TagDataDownloader(string site = "stackoverflow", int page=1)
+        {
+            this.client = new RestClient("https://api.stackexchange.com");
+            this.site = site;
+            this.page = page;
+        }
+
+        public List<TagData> Download()
+        {
+
+            var response = this.client.Execute<TagDataRequestResponse>(this.GetRequest());
+            return response.Data.items;
+
+        }
+
+        private RestRequest GetRequest()
+        {
+            var url = $"https://api.stackexchange.com/2.3/tags?pagesize=100&order=desc&sort=popular&site={this.site}&page={this.page}";
+            return new RestRequest(url, Method.GET);
+        }
+        
+    }
+}
