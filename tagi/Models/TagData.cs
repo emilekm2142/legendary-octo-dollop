82dd5998277745bc156a9270617c31025f0fb8dd
emil <emilm7843@gmail.com>
2021-09-28 15:37:50 +0200

    initial

---
+++ tagi/Models/TagData.cs
@@ -0,0 +1,12 @@
+﻿namespace tagi
+{
+    public class TagData
+    {
+        public bool has_synonyms { get; set; }
+        public bool is_moderator_only { get; set; }
+        public bool is_required { get; set; }
+        public long count { get; set; }
+        public string name { get; set; }
+
+    }
+}
---
+++ tagi/Models/TagDataRequestResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace tagi
+{
+    public class TagDataRequestResponse
+    {
+        public List<TagData> items { get; set; }
+        public bool has_more { get; set; }
+        public int quota_max { get; set; }
+        public int quota_remaining { get; set; }
+    }
+}
